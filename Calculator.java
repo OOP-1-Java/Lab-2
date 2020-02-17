3415b9ebf57abd616c4820d41cbfdab713de8387
mhpranta12 <60335123+mhpranta12@users.noreply.github.com>
2020-02-17 11:13:35 +0600

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+package calculatorDemo;
+
+public class Calculator {
+
+	public int sub(int n1, int n2) {
+
+		return n1 - n2;
+
+	}
+
+	public int add(int a, int b) {
+
+		return a + b;
+	}
+
+}
---
+++ CalculatorTest.java
@@ -0,0 +1,16 @@
+package calculatorDemo;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+
+		Calculator cal = new Calculator();
+		
+		int result = cal.add(2, 6);
+		
+		
+		System.out.print(result);
+		
+	}
+
+}
